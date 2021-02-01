dc85b603331710180e4cc658d46015d70f8f3d72
sezer karakaş <sezeroooo@hotmail.com>
2021-02-02 00:13:37 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Brand + " : " + car.Description + " : " + car.DailyPrice);
+            }
+
+            Console.WriteLine("**********************************************");
+
+            foreach (var car in carManager.GetById(5))
+            {
+                Console.WriteLine(car.Brand + " - BrandId: " + car.BrandId + " - CarId: " + car.CarId + " - ColorId: " + car.ColorId + " - " + car.ColorName + " - Price: " + car.DailyPrice + " - " + car.Description);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1, Brand="Eagle", BrandId=1, ColorName="White", ColorId=1, DailyPrice=20000, ModelYear=1994, Description="It is looking like an eagle."},
+                new Car{CarId=2, Brand="Hawk", BrandId=2, ColorName="Red", ColorId=2, DailyPrice=25000, ModelYear=1990, Description="It is looking like a hawk."},
+                new Car{CarId=3, Brand="Falcon", BrandId=3, ColorName="Blue", ColorId=3, DailyPrice=22500, ModelYear=1993, Description="It is looking like a falcon."},
+                new Car{CarId=4, Brand="Anatolia", BrandId=4, ColorName="White", ColorId=1, DailyPrice=30000, ModelYear=1976, Description="It is from Anatolia."},
+                new Car{CarId=5, Brand="Femrari", BrandId=5, ColorName="White", ColorId=1, DailyPrice=1000, ModelYear=2010, Description="It is a cheap car."},
+
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.CarId == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.Brand = car.Brand;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ColorName = car.ColorName;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
